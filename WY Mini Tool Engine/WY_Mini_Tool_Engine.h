ef11794e495666419df182f6aff1d0ca9159909d
Ilya Sakalou <ilya.sokolov@orangesoft.by>
2020-05-04 18:53:28 +0300

    Initial Commit

---
+++ WY Mini Tool Engine/WY_Mini_Tool_Engine.h
@@ -0,0 +1,19 @@
+//
+//  WY_Mini_Tool_Engine.h
+//  WY Mini Tool Engine
+//
+//  Created by Ilya Sakalou on 5/4/20.
+//  Copyright © 2020 Nirma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WY_Mini_Tool_Engine.
+FOUNDATION_EXPORT double WY_Mini_Tool_EngineVersionNumber;
+
+//! Project version string for WY_Mini_Tool_Engine.
+FOUNDATION_EXPORT const unsigned char WY_Mini_Tool_EngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WY_Mini_Tool_Engine/PublicHeader.h>
+
+
---
+++ WY Mini Tool EngineTests/WY_Mini_Tool_EngineTests.swift
@@ -0,0 +1,34 @@
+//
+//  WY_Mini_Tool_EngineTests.swift
+//  WY Mini Tool EngineTests
+//
+//  Created by Ilya Sakalou on 5/4/20.
+//  Copyright © 2020 Nirma. All rights reserved.
+//
+
+import XCTest
+@testable import WY_Mini_Tool_Engine
+
+class WY_Mini_Tool_EngineTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
